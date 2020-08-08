436d30b96a2d23634db561412aa6ec8d3797d15c
sondol <song4489z@naver.com>
2020-08-09 01:27:04 +0900

    add main file
    hello func

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"name":"song"})
+		return
+	})
+
+	router.Run(":80")
+}
